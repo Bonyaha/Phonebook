76acc59edbaa85d57154bc4a40db8df52e709129
Roman <romchikus1987@gmail.com>
2023-05-21 18:54:11 +0300

    updated

--- index.js
+++ index.js
@@ -1,68 +1,44 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-app.use(express.json()) //transforms JSON data of a request into a JavaScript object and then attaches it to the body property of the request object before the route handler is called
-
 const cors = require('cors')
-app.use(cors())
-
-app.use(express.static('build'))
-
 const morgan = require('morgan')
+const Person = require('./models/contact')
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
+app.use(express.json()) //transforms JSON data of a request into a JavaScript object and then attaches it to the body property of the request object before the route handler is called
 // Logging middleware based on the tiny configuration.
 app.use(morgan('tiny'))
 //shows the data sent in HTTP POST requests
 morgan.token('body', (req) => JSON.stringify(req.body))
-
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
-
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-const generatedId = () => {
-  const id = persons.length > 0 ? Math.floor(Math.random() * 10000) : 0
-  return id
-}
+app.use(express.static('build'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons) => response.json(persons))
 })
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((p) => p.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).send('Page not found')
-  }
+  Person.findById(request.params.id).then((contact) => response.json(contact))
 })
 app.get('/info', (request, response) => {
-  response.send(`<h3>Phonebook has info for ${persons.length} people</h3>
-    <p>${new Date()}</p> `)
+  Person.countDocuments({})
+    .then((count) => {
+      response.send(`<h3>Phonebook has info for ${count} people</h3>
+        <p>${new Date()}</p> `)
+    })
+    .catch((err) => {
+      console.log(err)
+      response.status(500).send('Internal Server Error')
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -87,17 +63,24 @@ app.post('/api/persons', (request, response) => {
       error: 'The number is missing.',
     })
   }
-  if (persons.find((p) => p.name === body.name)) {
-    return response.status(400).json({ error: 'name must be unique' })
-  }
-  const person = {
-    id: generatedId(),
-    name: body.name,
-    number: body.number,
-  }
-  persons = persons.concat(person)
-  response.json(person)
+  Person.exists({ name: body.name })
+    .then((nameExists) => {
+      if (nameExists) {
+        return response.status(400).json({ error: 'name must be unique' })
+      }
+      const person = new Person({
+        name: body.name,
+        number: body.number,
+      })
+      person.save().then((contact) => response.json(contact))
+    })
+    .catch((error) => {
+      console.error(error)
+      response.status(500).send('Internal Server Error')
+    })
 })
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
--- mongo.js
+++ mongo.js
@@ -14,12 +14,12 @@ const url = `mongodb+srv://Roman:${password}@cluster1.d7mltpu.mongodb.net/phoneb
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
+const contactSchema = new mongoose.Schema({
   name: String,
   number: String,
 })
 
-const Person = mongoose.model('Contact', noteSchema)
+const Person = mongoose.model('Contact', contactSchema)
 
 const contact = new Person({
   name,
